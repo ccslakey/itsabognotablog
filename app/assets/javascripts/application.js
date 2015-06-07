03e5a1219ae5711bc9d89364d3ead2c5f6c24767
ccslakey <cromwellslakey@gmail.com>
2015-06-07 10:41:57 -0700

    Can't get buttons to work :( bootstrap other things now

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap
+//= require jquery_ujs
+
+//= require_tree .
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
